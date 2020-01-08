ba600fbefd4afa921dc9c67b6fc2a03825e24521
Neal <704683538@qq.com>
2020-01-08 18:03:50 +0800

    feat: 3

--- app.js
+++ app.js
@@ -1,48 +1,9 @@
-const Koa = require('koa')
-const app = new Koa()
-
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const mongodbCon = require('./middleware/mongodb');
-app.use(mongodbCon());
-
-const index = require('./routes/index')
-const users = require('./routes/users')
-
-// error handler
-onerror(app)
-
-
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
-
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
-// routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+'use strict';
+var express = require('express');
+var PORT = 8888;
+var app = express();
+app.get('/', function (req, res) { 
+    res.send('Helloworld\n');
 });
-
-module.exports = app
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
